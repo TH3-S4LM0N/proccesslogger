43e3b6f16ba43ec57b3378adc19d137e3659e767
TH3-S4LM0N <blakeblankenship@ccawarriors.com>
2023-01-15 19:17:56 -0600

    partial loop logic

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
+
 cargo build --release
+cp ./etc/proccesslogger.service /etc/systemd/user
 cp ./target/release/proccesslogger /usr/bin/proccesslogger
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+use std::{collections::HashMap, fs::{File, create_dir, read_to_string}, hash::Hash, io::{Write, Read}};
+
 use chrono::Datelike;
 use config::DayConfig;
+use sysinfo::{Pid, Process, System, SystemExt};
 
 mod config;
 mod diff;
-mod dayloops;
+//mod time;
 
 const HOUR: std::time::Duration = std::time::Duration::from_secs(3600);
 const MINUTE: std::time::Duration = std::time::Duration::from_secs(60);
@@ -11,15 +14,20 @@ const BETWEEN_CHECKS: std::time::Duration = std::time::Duration::from_secs(60);
 const LOGFILE: &str = "~/.processlogfile";
 
 fn main() {
+    let now = chrono::Local::now();
+    dbg!(now);
+    let nowutc = chrono::Utc::now();
+    dbg!(nowutc);
+    panic!();
+
     // parse arg
     let args: Vec<String> = std::env::args().collect();
-    if args.len() <= 1 {
-        
-    }
+    if args.len() <= 1 {}
 
     // get xdg config dir
     // the intention is that its run as a userspace systemd module
-    let xdg = xdg::BaseDirectories::with_prefix("processlogger").expect("Failed to create xdg dirs");
+    let xdg =
+        xdg::BaseDirectories::with_prefix("processlogger").expect("Failed to create xdg dirs");
 
     // get config
     let cfg = config::load_config(&xdg.get_config_file("config.toml"));
@@ -37,12 +45,30 @@ fn main() {
             "Fri" => &cfg.friday,
             "Sat" => &cfg.saturday,
             "Sun" => &cfg.sunday,
-            _ => unreachable!("{} is invalid!", day.as_str())
+            _ => unreachable!("{} is invalid!", day.as_str()),
         };
 
         // loop until it is daycfg.start
-        loop {
-            println!("a{}a", daycfg.start);
+        while &format!("{}", chrono::Local::now().format("%H:%M")) != &daycfg.start {
+            std::thread::sleep(MINUTE);
+        }
+
+        let file = File::open(LOGFILE).expect("Failed to open file");
+        let mut contents: String;
+        file.read_to_string(&mut contents).expect("Failed to read file");
+        file.write_all(format!("{}\n---{}---", contents, chrono::Local::now()).as_bytes()).expect("Failed to write file.");
+
+
+        let mut last_procs: &HashMap<Pid, Process>;
+        // until the end
+        while &format!("{}", chrono::Local::now().format("%H:%M")) != &daycfg.end {
+            let mut system = System::new_all();
+            system.refresh_all();
+            let procs = system.processes();
+
+            let diff = diff::diff_hashmap(last_procs, procs);
+
+            let file = File::open(LOGFILE).expect("Failed to create file");
         }
     }
 }
