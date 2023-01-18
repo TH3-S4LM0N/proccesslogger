717781177b25b073d9f4c8a408d65dbbfe3acef5
TH3-S4LM0N <blakeblankenship@ccawarriors.com>
2023-01-18 17:30:31 -0600

    restructure

---
+++ target/debug/deps/pl_daemon-4b276c73c65aec1d.d
@@ -0,0 +1,5 @@
+/home/salmon/projects/rust/proccesslogger/target/debug/deps/pl_daemon-4b276c73c65aec1d.rmeta: pl-daemon/src/main.rs
+
+/home/salmon/projects/rust/proccesslogger/target/debug/deps/pl_daemon-4b276c73c65aec1d.d: pl-daemon/src/main.rs
+
+pl-daemon/src/main.rs:
---
+++ target/debug/deps/pl_web-283ceace6bb2f5b5.d
@@ -0,0 +1,5 @@
+/home/salmon/projects/rust/proccesslogger/target/debug/deps/pl_web-283ceace6bb2f5b5.rmeta: pl-web/src/main.rs
+
+/home/salmon/projects/rust/proccesslogger/target/debug/deps/pl_web-283ceace6bb2f5b5.d: pl-web/src/main.rs
+
+pl-web/src/main.rs:
